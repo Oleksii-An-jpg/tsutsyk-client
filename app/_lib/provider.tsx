ae94b4b6342a192b8ccae95cdb5268ef16377e23
oleksii.an <oleksii.a@gocheetah.com>
2026-08-10 01:52:51 +0300

    attempt to fix hydration errors #1

--- app/_lib/provider.tsx
+++ app/_lib/provider.tsx
@@ -4,6 +4,55 @@ import {FC, PropsWithChildren, useEffect, useMemo} from "react";
 import {createApolloClient} from "@/app/_lib/apollo";
 import {ApolloProvider} from "@apollo/client/react";
 
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { useServerInsertedHTML } from "next/navigation";
+import type React from "react";
+import { useState } from "react";
+
+const EmotionRegistry: FC<PropsWithChildren> = ({
+                                            children,
+                                        }) => {
+    const [{ cache, flush }] = useState(() => {
+        const cache = createCache({ key: "css" });
+        cache.compat = true;
+        const prevInsert = cache.insert;
+        let inserted: string[] = [];
+        cache.insert = (...args) => {
+            const serialized = args[1];
+            if (cache.inserted[serialized.name] === undefined) {
+                inserted.push(serialized.name);
+            }
+            return prevInsert(...args);
+        };
+        const flush = () => {
+            const prevInserted = inserted;
+            inserted = [];
+            return prevInserted;
+        };
+        return { cache, flush };
+    });
+
+    useServerInsertedHTML(() => {
+        const names = flush();
+        if (names.length === 0) return null;
+        let styles = "";
+        for (const name of names) {
+            styles += cache.inserted[name];
+        }
+        return (
+            <style
+                key={cache.key}
+                data-emotion={`${cache.key} ${names.join(" ")}`}
+                // biome-ignore lint/security/noDangerouslySetInnerHtml: Avoid hydration errors
+                dangerouslySetInnerHTML={{ __html: styles }}
+            />
+        );
+    });
+
+    return <CacheProvider value={cache}>{children}</CacheProvider>;
+}
+
 const Provider: FC<PropsWithChildren> = ({ children }) => {
     const client = useMemo(() => {
         return createApolloClient();
@@ -20,7 +69,9 @@ const Provider: FC<PropsWithChildren> = ({ children }) => {
     }, [client]);
 
     return <ApolloProvider client={client}>
-        {children}
+        <EmotionRegistry>
+            {children}
+        </EmotionRegistry>
     </ApolloProvider>
 }
 
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactCompiler: true,
+    experimental: {
+        optimizePackageImports: ["@chakra-ui/react"],
+    },
+    compiler: {
+        emotion: true,
+    },
 };
 
 export default nextConfig;
